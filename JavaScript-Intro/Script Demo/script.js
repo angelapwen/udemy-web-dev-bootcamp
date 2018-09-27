88049ad980013fa749c358e8f4c629a2da615006
Unknown <wena@oregonstate.edu>
2018-09-27 19:22:47 +0800

    Add intro JS demo

---
+++ JavaScript-Intro/Script Demo/script.js
@@ -0,0 +1,5 @@
+alert("Hello from the JS file!");
+
+var name = prompt("What is your name?");
+alert("Hello " + name + ", nice to meet you!");
+console.log("Also happy to see you in the console, " + name);
