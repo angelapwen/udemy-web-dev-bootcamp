3c790e4b7bd3d6b9b6b19e5c6fdab9bbd051aef1
Unknown <wena@oregonstate.edu>
2018-09-28 15:43:10 +0800

    Add annoy-ometer while loop exercise

---
+++ js-control-flow/annoyomatic/annoy.js
@@ -0,0 +1,7 @@
+var input = prompt("Are we there yet?");
+
+while (input.indexOf("yes") === -1 && input.indexOf("yeah") === -1) {
+	input = prompt("Are we there yet?");
+}
+
+alert("Yay, we finally made it!");
