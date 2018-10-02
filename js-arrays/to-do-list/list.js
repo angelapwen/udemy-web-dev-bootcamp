224e6d38aacf2b0edeec0bb51a87354f344338bc
Unknown <wena@oregonstate.edu>
2018-10-02 19:18:03 +0800

    Iterate using forEach, delete element with index

--- js-arrays/to-do-list/list.js
+++ js-arrays/to-do-list/list.js
@@ -9,16 +9,41 @@ window.setTimeout(function() {
 	// Run app while user does not choose to quit 
 	while (input !== "quit") {
 		if (input === "new") {
-			var newItem = prompt("Enter new to-do-item:");
-			todos.push(newItem);
+			addTodo();	
 		}
 		else if (input === "list") {
-			console.log(todos);
+			listTodos();
+		}
+		else if (input === "delete") {
+			deleteTodo();
 		}
 		// Prompt user for input
-		var input = prompt("What would you like to do?");
+		input = prompt("What would you like to do?");
 	}
+
 	console.log("Successfully quit the app.");
 
+	function listTodos() {
+		console.log("***********");
+		// Print each element individually with array element
+		todos.forEach(function(todo, index) {
+			console.log(index + ": " + todo);
+		});
+		console.log("***********");
+	}
+
+	function addTodo() {
+		var newItem = prompt("Enter new to-do-item:");
+		todos.push(newItem);
+		console.log(newItem + "  added to-do");
+	}	
+
+	function deleteTodo() {
+		var index = prompt("Which index to delete?");
+		// Use splice() method to delete 1 element after a specific point
+		todos.splice(index,1);
+		console.log("Item at index " + index + " deleted");
+	}
 
 }, 500);
+
