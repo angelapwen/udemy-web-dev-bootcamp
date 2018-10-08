3c6af228b1cde8cc4221f916ba271d2ddd29439e
Unknown <wena@oregonstate.edu>
2018-10-08 21:02:33 +0800

    Add reset functionality, green color for win

--- dom/score-keeper/scorekeeper.js
+++ dom/score-keeper/scorekeeper.js
@@ -1,18 +1,21 @@
 let p1Button = document.querySelector("#p1");
 let p2Button = document.getElementById("p2");
+let resetButton = document.getElementById("reset");
 let p1Score = 0
 let p2Score = 0;
 let p1Display = document.querySelector("#p1Display");
 let p2Display = document.querySelector("#p2Display");
+let numInput = document.querySelector("#input");
 let gameOver = false;
-var winningScore = 5;
+let winningScore = 5;
 
 p1Button.addEventListener("click", function() {
 	if (!gameOver) {
 		p1Score++;
 		p1Display.textContent = p1Score;
 		if (p1Score === winningScore) {
-		gameOver = true;
+			gameOver = true;
+			p1Display.classList.add("winner");
 		}
 	}
 });
@@ -22,8 +25,17 @@ p2Button.addEventListener("click", function() {
 		p2Score++;
 		p2Display.textContent = p2Score;
 		if (p2Score === winningScore) {
-		gameOver = true;
+			gameOver = true;
+			p2Display.classList.add("winner");
+
 		}
 	}
 });
 
+resetButton.addEventListener("click", function() {
+	p1Score = p2Score = 0;
+	p1Display.textContent = p2Display.textContent = 0;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+	gameOver = false;
+});
