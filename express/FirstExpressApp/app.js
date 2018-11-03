8563103fa0335b53d8bc78b40e49a33d508f7a3f
Unknown <wena@oregonstate.edu>
2018-11-03 15:16:23 +0800

    Practice with * and route parameters

--- express/FirstExpressApp/app.js
+++ express/FirstExpressApp/app.js
@@ -17,7 +17,19 @@ app.get("/dog",function(req,res) {
 	res.send("MEOW!");
 });
 
+app.get("/r/:subredditName", function(req,res) {
+	let subreddit = req.params.subredditName;
+	res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT");
+});
 
+app.get("/r/:subredditName/comments/:id/:title", function(req,res) {
+	console.log(req.params);
+	res.send("Welcome to the comments page");
+});
+
+app.get("*", function(req,res) {
+	res.send("YOU ARE A STAR!");
+});
 
 // Tell Express to listen for requests (start server)
 app.listen(3000, function() {
