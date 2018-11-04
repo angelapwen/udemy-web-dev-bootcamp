1b448d0a16a7a20371a052d090cc24c736b6d206
Unknown <wena@oregonstate.edu>
2018-11-04 18:56:19 +0800

    Add first .ejs files

---
+++ express/ejs-demo/app.js
@@ -0,0 +1,16 @@
+let express = require("express");
+let app = express();
+
+app.get("/", function(req, res) {
+	res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:thing", function(req, res) {
+	let thing = req.params.thing;
+	res.render("love.ejs", {thingVar: thing});
+});
+
+
+app.listen(3000, function() {
+	console.log("Server is listening!");
+})
