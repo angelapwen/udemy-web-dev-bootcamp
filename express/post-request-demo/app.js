806c666fc771af28aace08ac55e02c33cbdff354
Unknown <wena@oregonstate.edu>
2018-11-06 22:11:20 +0800

    Add body-parser for POST requests

--- express/post-request-demo/app.js
+++ express/post-request-demo/app.js
@@ -1,19 +1,28 @@
 let express = require("express");
 let app = express();
+// Import body parser package to parse body into JavaScript object
+let bodyParser = require("body-parser");
+
+// Tell Express to use body parser
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+// Move friends outside routes for access
+let friends = ["Julian", "Isobel", "Justine", "Elena", "Alex"];
 
 app.get("/", function(req, res) {
 	res.render("Home");
 });
 
-app.get("/friends", function(req, res) {
-	let friends = ["Julian", "Isobel", "Justine", "Elena", "Alex"];
-	res.render("friends", {friends: friends});
+app.post("/addFriend", function(req, res) {
+	let newFriend = req.body.newfriend;
+	friends.push(newFriend); // Push new friend to end of array
+	res.redirect("/friends"); // Redirect to /friends route
 });
 
-app.post("/addFriend", function(req, res) {
-	res.send("You have reached the post route");
+app.get("/friends", function(req, res) {
+	res.render("friends", {friends: friends});
 });
 
 app.listen(3000,function(){
