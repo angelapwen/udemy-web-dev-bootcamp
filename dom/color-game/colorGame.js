18d520c8bd110a011d67faaa04a25ad33505071a
Unknown <wena@oregonstate.edu>
2019-01-10 21:46:46 +0800

    Add win/lose functionality, randomize color choice

--- dom/color-game/colorGame.js
+++ dom/color-game/colorGame.js
@@ -8,8 +8,9 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = randomColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor; 
 for (let i = 0; i < squares.length; i++) {
@@ -22,10 +23,27 @@ for (let i = 0; i < squares.length; i++) {
 		var clickedColor = this.style.backgroundColor;
 		// Compare to picked color
 		if (clickedColor === pickedColor) {
-			alert("Correct!");
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
 		}
 		else {
-			alert("Wrong!");
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
 		}
 	});
+}
+
+function changeColors(color) {
+	// Loop through all squares
+	for (let i = 0; i < squares.length; i++) {
+		// Change each color to match correct color
+		squares[i].style.background = color;
+	}
+}
+
+function randomColor() {
+	// Pick a random number between 1 and max index of array
+	var random = Math.floor(Math.random() * colors.length)
+	// Use the number to access 
+	return colors[random];
 }
