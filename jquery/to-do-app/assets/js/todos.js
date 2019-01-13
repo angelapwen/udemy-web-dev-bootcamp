b9bf5ce6c5c890403cc0f6751f44df374f8fd231
Unknown <wena@oregonstate.edu>
2019-01-13 19:22:16 +0800

    Add input form toggling

--- jquery/to-do-app/assets/js/todos.js
+++ jquery/to-do-app/assets/js/todos.js
@@ -22,3 +22,7 @@ $("input[type='text']").keypress(function(event) {
 		$("ul").append("<li><span><i class ='far fa-trash-alt'></i></span> " + todoText + "</li>");
 	}
 });
+
+$("#toggle-form").click(function() {
+	$("input[type='text']").fadeToggle();
+});
