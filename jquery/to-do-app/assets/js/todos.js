7be3f1986a3ea4e6e477140bef813e3dfab0bb26
Unknown <wena@oregonstate.edu>
2019-01-13 18:49:22 +0800

    Add new list item addition

--- jquery/to-do-app/assets/js/todos.js
+++ jquery/to-do-app/assets/js/todos.js
@@ -1,12 +1,24 @@
 // Check off specific to-dos by clicking
-$("li").click(function() {
+$("ul").on('click', "li", function() {
 	$(this).toggleClass("completed");
 });  
 
 // Click on X to delete to-do
-$("span").click(function(event) {
+$("ul").on('click', "span", function(event) {
 	$(this).parent().fadeOut(500, function() {
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+// Add listener to input to fire when we hit enter
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13) {
+		// Extract value that was input 
+		var todoText = $(this).val();
+		// Clear input box
+		$(this).val("");
+		// Create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
 });
