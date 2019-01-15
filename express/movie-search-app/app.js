abbd11ce8348ef8233f9a2b136df1c6ade8da343
Unknown <wena@oregonstate.edu>
2019-01-15 15:51:34 +0800

    Add search form, minor styling

--- express/movie-search-app/app.js
+++ express/movie-search-app/app.js
@@ -3,8 +3,13 @@ const app = express();
 const request = require("request");
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+	res.render("search");
+});
+
 app.get("/results", function(req, res) {
-	request("https://www.omdbapi.com/?s=california&apikey=thewdb", function(error, response, body) {
+	let searchTerm = req.query.search;
+	request(`https://www.omdbapi.com/?s=${searchTerm}&apikey=thewdb`, function(error, response, body) {
 		if(!error && response.statusCode == 200) {
 			let data = JSON.parse(body);
 			res.render("results", {data: data});
