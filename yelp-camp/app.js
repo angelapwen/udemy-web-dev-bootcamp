471cde077d95f9d33e25e9e580568991fa7f3f71
Unknown <wena@oregonstate.edu>
2019-01-16 16:25:16 +0800

    Style header jumbotron, grid in campgrounds page

--- yelp-camp/app.js
+++ yelp-camp/app.js
@@ -9,6 +9,12 @@ app.set("view engine", "ejs");
 	var campgrounds = [
 		{name: "Salmon Creek", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
 		{name: "Granite Hill", image: "https://farm4.staticflickr.com/3911/14707566622_af236f9b65.jpg"},
+		{name: "Mountain Goat's Rest", image: "https://farm3.staticflickr.com/2931/14128269785_f27fb630f3.jpg"},
+		{name: "Salmon Creek", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
+		{name: "Granite Hill", image: "https://farm4.staticflickr.com/3911/14707566622_af236f9b65.jpg"},
+		{name: "Mountain Goat's Rest", image: "https://farm3.staticflickr.com/2931/14128269785_f27fb630f3.jpg"},
+		{name: "Salmon Creek", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
+		{name: "Granite Hill", image: "https://farm4.staticflickr.com/3911/14707566622_af236f9b65.jpg"},
 		{name: "Mountain Goat's Rest", image: "https://farm3.staticflickr.com/2931/14128269785_f27fb630f3.jpg"}
 	];
 
