98618e391a538aaa4decb41555df249de91d07bc
Unknown <wena@oregonstate.edu>
2019-01-22 17:33:02 +0800

    Add SHOW route, add description to camps

--- yelp-camp/app.js
+++ yelp-camp/app.js
@@ -10,30 +10,33 @@ app.set("view engine", "ejs");
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
 	name: String,
-	image: String
+	image: String,
+	description: String
 });
 
 // Compile into model
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-// Campground.create(
-// 	{
-// 		name: "Granite Hill", 
-// 		image: "https://farm4.staticflickr.com/3911/14707566622_af236f9b65.jpg"
-// 	}, function(err, campground) {
-// 		if(err) {
-// 			console.log(err);
-// 		}
-// 		else {
-// 			console.log("Newly created campground: ");
-// 			console.log(campground);
-// 		}
-// 	});
+Campground.create(
+	{
+		name: "Granite Hill", 
+		image: "https://farm4.staticflickr.com/3911/14707566622_af236f9b65.jpg",
+		description: "This is a new campground, no bathrooms yet but is beautiful."
+	}, function(err, campground) {
+		if(err) {
+			console.log(err);
+		}
+		else {
+			console.log("Newly created campground: ");
+			console.log(campground);
+		}
+	});
 
 app.get("/", function(req, res) {
 	res.render("landing");
 });
 
+// INDEX ROUTE - show all routes
 app.get("/campgrounds", function(req, res) {
 	// Get all campgrounds from database
 	Campground.find({}, function(err, allCampgrounds) {
@@ -41,16 +44,18 @@ app.get("/campgrounds", function(req, res) {
 			console.log("ERROR");
 		}
 		else {
-			res.render("campgrounds", {campgrounds: allCampgrounds});
+			res.render("index", {campgrounds: allCampgrounds});
 		}
 	});
 });
 
+// CREATE ROUTE - add new campground 
 app.post("/campgrounds", function (req, res) {
 	// Get data from form and add to campgrounds array
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name: name, image: image};
+	var desc = req.body.description;
+	var newCampground = {name: name, image: image, description: desc};
 	// Create new campground and save to database
 	Campground.create(newCampground, function(err, newlyCreatedCampground) {
 		if(err) {
@@ -63,8 +68,23 @@ app.post("/campgrounds", function (req, res) {
 	})
 });
 
+// NEW - show form to create new campground
 app.get("/campgrounds/new", function (req, res) {
 	res.render("new");
+});
+
+// SHOW - show smore info about one campground 
+app.get("/campgrounds/:id", function(req, res) {
+	// Find campground with provided ID
+	Campground.findById(req.params.id, function(err, foundCampground) {
+		if(err) {
+			console.log(err);
+		}
+		else {
+			// Render show template 
+			res.render("show", {campground: foundCampground});
+		}
+	});	
 });
 
 // Tell Express to listen for requests (start server)
