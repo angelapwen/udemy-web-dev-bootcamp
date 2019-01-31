4aa3d76e1a33080c3532e5276777d8b6e74e23e8
Unknown <wena@oregonstate.edu>
2019-01-31 18:44:49 +0700

    Save new comments to database

--- yelp-camp/app.js
+++ yelp-camp/app.js
@@ -3,7 +3,9 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
+const Comment = require("./models/comment");
 const seedDB = require("./seeds");
+
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
@@ -79,6 +81,31 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
 			res.render("comments/new", {campground: campground});
 		}
 	})
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+	// Look up campground using ID
+	Campground.findById(req.params.id, function(err, campground) {
+		if(err) {
+			console.log(err);
+			res.redirect("/campgrounds");
+		}
+		else {
+			// Create new comment
+			Comment.create(req.body.comment, function(err, comment) {
+				if(err) {
+					console.log(err);
+				}
+				else {
+					// Connect new comment to campground
+					campground.comments.push(comment);
+					campground.save(); 
+					// Redirect campground to show page
+					res.redirect("/campgrounds/" + campground._id);
+				}
+			});
+		}
+	});
 });
 
 // Tell Express to listen for requests (start server)
