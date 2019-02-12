954752d80d15443852598ebed70ea8c6d109e177
Unknown <wena@oregonstate.edu>
2019-02-12 15:03:24 +0800

    Update and edit campground

--- yelp-camp/app.js
+++ yelp-camp/app.js
@@ -8,6 +8,7 @@ const User = require("./models/user");
 const seedDB = require("./seeds");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const methodOverride = require("method-override");
 
 // Requiring routes
 const commentRoutes = require("./routes/comments"),
@@ -18,6 +19,7 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true})
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 // seedDB();
 
 // Passport Configuration
